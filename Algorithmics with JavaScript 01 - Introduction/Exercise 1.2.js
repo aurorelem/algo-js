9b64321035032ce6413e46f15ff788abd2ea8a70
aurorelem <110966715+aurorelem@users.noreply.github.com>
2022-11-25 14:42:07 +0100

    Update Exercise 1.2.js

--- Algorithmics with JavaScript 01 - Introduction/Exercise 1.2.js
+++ Algorithmics with JavaScript 01 - Introduction/Exercise 1.2.js
@@ -1,4 +1,6 @@
-var name = "aurore"
-console.log(name)
+var name = "Gérard"
+var firstName = "Lambert"
+var city = "Paris"
+console.log("Your name is " + name + " " + firstName + " and you live in " + city)
 
- =)"aurore"
+ =)"Your name is Gérard Lambert and you live in Paris"
