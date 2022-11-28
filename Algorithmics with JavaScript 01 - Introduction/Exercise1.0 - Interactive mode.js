2d960f08cdc8156443dec2be13dca135e92cb5e3
aurorelem <aurorelemaire.pro@gmail.com>
2022-11-28 09:21:14 +0100

    exercices 1

--- Algorithmics with JavaScript 01 - Introduction/Exercise1.0 - Interactive mode.js
+++ Algorithmics with JavaScript 01 - Introduction/Exercise1.0 - Interactive mode.js
@@ -1,1 +1,23 @@
+Addition:
+var x = 12
+var y = 12
+console.log(x+y)
+  =24
 
+Soustraction:
+var x = 12
+var y = 12
+console.log(x-y)
+  =0
+
+Multiplication:
+var x = 12
+var y = 12
+console.log(x/y)
+  =1
+
+Division:
+var x = 12
+var y = 12
+console.log(x*y)
+  =144
